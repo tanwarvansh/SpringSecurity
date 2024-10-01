05c8cdef64f4a6e484336fe636da84e945c1baec
Vansh Tanwar <vanshtanwar9@gmail.com>
2024-10-01 11:13:38 +0530

    adding basic Spring Boot project

---
+++ SpringSecSection1/src/main/java/com/vansh/SpringSecSection1/SpringSecSection1Application.java
@@ -0,0 +1,13 @@
+package com.vansh.SpringSecSection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecSection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecSection1Application.class, args);
+	}
+
+}
