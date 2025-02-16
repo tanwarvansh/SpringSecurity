6d48316caf3ee2e07c310ce4022a1f50e8204986
Vansh Tanwar <vanshtanwar9@gmail.com>
2025-02-16 09:27:02 +0530

    adding basic Spring Boot project

--- SpringSecSection1/src/main/java/com/vansh/SpringSecSection1/EazyBackBackendApplication.java
+++ SpringSecSection1/src/main/java/com/vansh/SpringSecSection1/EazyBackBackendApplication.java
@@ -3,11 +3,16 @@ package com.vansh.SpringSecSection1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class SpringSecSection1Application {
+public class EazyBackBackendApplication {
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecSection1Application.class, args);
+		SpringApplication.run(EazyBackBackendApplication.class, args);
+
 	}
 
 }
